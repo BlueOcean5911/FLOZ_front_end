cb4639013ee5ed5fb07bd583dcf3e25245f76fef
BlueOcean5911 <dev.freshworks@gmail.com>
2023-11-02 16:09:56 +0500

    added cursor pointer

--- components/ProjectNavigator/ProjectNavigator.tsx
+++ components/ProjectNavigator/ProjectNavigator.tsx
@@ -30,7 +30,7 @@ export default function ProjectNavigator(props: { pId?: string }) {
       <div className="w-72">
         <Listbox value={selected.id} onChange={setSelected}>
           <div className="relative mt-1">
-            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <Listbox.Button className="relative w-full cursor-pointer rounded-lg bg-white py-2 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
               <span className="block truncate font-bold">{selected.name}</span>
               <span className="pointer-events-none  absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpIcon

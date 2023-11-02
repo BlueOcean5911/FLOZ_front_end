28d7c7db1114cfd18b1d714efed719a5c17e5748
BlueOcean5911 <dev.freshworks@gmail.com>
2023-11-02 16:14:03 +0500

    Added input fields

--- components/ProjectDetail/ProjectDetail.tsx
+++ components/ProjectDetail/ProjectDetail.tsx
@@ -1,5 +1,16 @@
 export default function ProjectDetail(props: { pId: string }) {
   const { pId } = props;
 
-  return <div className="mt-12 flex justify-between">Project DEtails</div>;
+  return (
+    <div className="mt-12 flex justify-between">
+      <div className="flex gap-4">
+        <div>
+          <input type="file" placeholder="Upload" className="w-72" />
+        </div>
+        <div>
+          <input type="file" placeholder="Upload" className="w-72" />
+        </div>
+      </div>
+    </div>
+  );
 }

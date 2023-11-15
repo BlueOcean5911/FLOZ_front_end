b4418ba6b7eed354c1a9a66b8c65241f51c634bb
BlueOcean5911 <dev.freshworks@gmail.com>
2023-11-15 20:49:40 +0530

    dashboard-landing

--- components/ProjectPanel/ProjectPanel.tsx
+++ components/ProjectPanel/ProjectPanel.tsx
@@ -14,9 +14,13 @@ export default function ProjectPanel({
   data: { id: any; name: any }[] | null;
 }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [allProjects, setAllProjects] = useState<
-    { id: any; name: any }[] | null
-  >(data);
+  const [allProjects, setAllProjects] = useState<{ id: any; name: any }[] | null>(data);
+  const [meetings, setMeetings] = useState([
+    { id: 1, time: '10:00 AM', title: 'Phone call with GC' },
+    { id: 2, time: '10:00 AM', title: 'Phone call with HVAC' },
+    { id: 3, time: '10:00 AM', title: 'Phone call with GC' },
+    { id: 4, time: '10:00 AM', title: 'Phone call with client' }
+  ] as any);
 
   function closeModal() {
     setIsOpen(false);
@@ -41,9 +45,11 @@ export default function ProjectPanel({
         name: projectName.toString(),
       };
       setAllProjects((prevProjects) => {
+
         if (prevProjects) {
           return [...prevProjects, newEntry];
         }
+        console.log(newEntry);
         return [newEntry];
       });
       setIsOpen(false);
@@ -51,24 +57,269 @@ export default function ProjectPanel({
   };
 
   return (
-    <div className="relative flex w-full items-center justify-between">
-      <div className="flex gap-x-4 overflow-x-auto">
-        {allProjects?.length === 0 ? (
-          <p>No Projects</p>
-        ) : (
-          allProjects?.map((project: { id: string; name: string }) => (
-            <Link
-              href={`/home/${project.id}`}
-              className="flex rounded-md border border-neutral-300 px-4"
-              key={project.id}
-            >
-              <h4 className="text-lg font-bold">{project.name}</h4>
-            </Link>
-          ))
-        )}
+    <div className="w-full items-center justify-between">
+      <div className="flex">
+        <div className="w-[910px] border rounded border-stone-300 px-3 py-3 bg-white mr-4" >
+
+          <div className="flex justify-between">
+            <div className="flex">
+              <h3 className="my-auto pr-2 font-bold text-sm">Project</h3>
+              <p className="my-auto text-sm" >As of today at 10:54 AM</p>
+            </div>
+            <div className="flex">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                placeholder="Search for the project"
+                className={`w-full rounded-md border p-2 px-4 outline-none `}
+              />
+              <button style={{ color: "#349989", borderRadius: "4px", border: "1px solid var(--Tone, #349989)", background: "var(--foundation-gray-neutral-100, #FFF)" }} type="button" onClick={openModal} className="right-0 top-0 ms-4 rounded-md border border-neutral-300 bg-gray-700 px-4 text-lg font-bold text-white" >
+                New
+              </button>
+            </div>
+          </div>
+
+
+          <div>
+            <div className="min-h-screen">
+
+              <div>
+                <div className="mt-1">
+                  <div className="bg-white rounded-md">
+                    <div>
+                      <h2 className="mb-4 text-sm font-bold title_color">This month</h2>
+                      <div>
+                        {allProjects?.length === 0 ? (
+                          <p>No Projects</p>
+                        ) :
+                          (
+                            <div>
+                              <div className="grid grid-cols-6 bg-gray-100 py-2 px-4 text-black-400 font-bold text-md">
+                                <div>
+                                  <input type="checkbox" className="" id="checkbox" />
+                                </div>
+                                <div><span>Name</span></div>
+                                <div><span className="m0-important" >Phase</span></div>
+                                <div><span>Due Date</span></div>
+                                <div><span></span></div>
+                                <div><span></span></div>
+                              </div>
+                              <div>
+
+                                {allProjects?.map((project: { id: string; name: string }) => (
+                                  <div className="grid grid-cols-6 border-t text-sm px-4 font-normal mt-4 space-x-4">
+                                    <div>
+                                      <input type="checkbox" className="" id="checkbox" />
+                                    </div>
+
+                                    <div className="m0-important">
+                                      <span className="m0-important f-small">{project.name}</span>
+                                    </div>
+                                    <div className="m0-important f-small">
+                                      <span>phase</span>
+                                    </div>
+                                    <div className="m0-important f-small">
+                                      <span>28/12/2021</span>
+                                    </div>
+                                    <div className="m0-important f-small">
+                                      <span className="title_color"> <Link href={`/home/${project.id}`} key={project.id}><h4 className="text-sm f-small">Go to project</h4>
+                                      </Link></span>
+                                    </div>
+                                    <div className="mx-0">
+                                      <select style={{ float: 'right' }}>
+                                        <option value={''}></option>
+                                        <option value={''} >Admin</option>
+                                        <option value={''}>User</option>
+                                      </select>
+                                    </div>
+                                  </div>
+                                ))}
+
+                              </div>
+                            </div>
+                          )
+                        }
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-4 bg-white rounded-md">
+                    <div>
+                      <h2 className="mb-4 text-sm font-bold title_color">Next month</h2>
+                      <div>
+                        {allProjects?.length === 0 ? (
+                          <p>No Projects</p>
+                        ) :
+                          (
+                            <div>
+                              <div className="grid grid-cols-6 bg-gray-100 py-2 px-4 text-black-400 font-bold text-md">
+                                <div>
+                                  <input type="checkbox" className="" id="checkbox" />
+                                </div>
+                                <div><span>Name</span></div>
+                                <div><span className="m0-important" >Phase</span></div>
+                                <div><span>Due Date</span></div>
+                                <div><span></span></div>
+                                <div><span></span></div>
+                              </div>
+                              <div>
+
+                                {allProjects?.map((project: { id: string; name: string }) => (
+                                  <div className="grid grid-cols-6 border-t text-sm px-4 font-normal mt-4 space-x-4">
+                                    <div>
+                                      <input type="checkbox" className="" id="checkbox" />
+                                    </div>
+
+                                    <div className="m0-important f-small">
+                                      <span className="m0-important">{project.name}</span>
+                                    </div>
+                                    <div className="m0-important f-small">
+                                      <span>phase</span>
+                                    </div>
+                                    <div className="m0-important f-small">
+                                      <span>28/12/2021</span>
+                                    </div>
+                                    <div className="m0-important f-small">
+                                      <span className="title_color"> <Link href={`/home/${project.id}`} key={project.id}><h4 className="text-sm f-small">Go to project</h4>
+                                      </Link></span>
+                                    </div>
+                                    <div >
+                                      <select style={{ float: 'right' }}>
+                                        <option value={''}></option>
+                                        <option value={''} >Admin</option>
+                                        <option value={''}>User</option>
+                                      </select>
+                                    </div>
+                                  </div>
+                                ))}
+
+                              </div>
+                            </div>
+                          )
+                        }
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                <span className="title_color"> <Link href={`/home/`} ><h4 className="view-all f-small text-sm">View All</h4>
+                                      </Link></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+        <div className="w-[428px] border rounded border-stone-300 px-3 py-3 bg-white" >
+          <div className="flex justify-between">
+            <div className="flex">
+              <h3 className="my-auto pr-2 font-bold text-sm">Meetings</h3>
+            </div>
+            <div className="flex">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                placeholder="Search for the project"
+                className={`w-full rounded-md border p-2 px-4 outline-none `}
+              />
+              <button style={{ color: "#349989", borderRadius: "4px", border: "1px solid var(--Tone, #349989)", background: "var(--foundation-gray-neutral-100, #FFF)" }} type="button" onClick={openModal} className="right-0 top-0 ms-4 rounded-md border border-neutral-300 bg-gray-700 px-4 text-lg font-bold text-white" >
+                New
+              </button>
+            </div>
+          </div>
+
+
+          <div>
+            <div className="min-h-screen">
+              <div>
+                <div className="mt-4">
+                  <div className="bg-white rounded-md">
+                    <div>
+
+                      {/* <div x-data="{ open: false }" className="min-h-screen bg-gray-50 py-6 flex flex-col items-center justify-center relative overflow-hidden sm:py-12">
+                        <div className="p-4 bg-blue-100 w-1/2 rounded flex justify-between items-center">
+                          <div className="flex items-center gap-2">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                            </svg>
+                            <h4 className="font-medium text-sm text-blue-500">Today</h4>
+                          </div>
+                        </div>
+                        <div className="w-1/2 bg-white p-4 ">
+                          <h4 className="text-sm text-slate-400">Now you can earn bitcoin in your wallet just by referring coinx to one of your friend.</h4>
+                          <button className="bg-blue-500 p-2 text-sm text-white font-bold rounded mt-4">Refer now</button>
+                        </div>
+                      </div> */}
+
+                      <div>
+                        {meetings?.length === 0 ? (
+                          <p>No Projects</p>
+                        ) :
+                          (
+                            <div>
+                              {/* <div className="grid grid-cols-3 bg-white-100 py-2 px-4 text-black-400 font-bold text-md">
+                                <div className="flex">
+                                  <svg xmlns="http://www.w3.org/2000/svg" className="mx-2 mt-1 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                                  </svg>Today
+                                </div>
+                              </div> */}
+                              <div>
+                                <section className="accordion">
+
+                                  <div className="tab bg-white-100 py-2 px-4 text-black-400 text-md">
+                                    <label for="cb2" className="tab__label font-bold">Today</label>
+                                    <input type="checkbox" name="accordion-1" id="cb2" />
+                                    <div className="tab__content">
+                                    {meetings?.map((meetings: { id: string; title: string, time: string }) => (
+                                  <div className="grid grid-cols-3 border-t text-sm px-4 font-small mt-4 space-x-4">
+                                    <div className="m0-important f-small">
+                                      <span>{meetings.time}</span>
+                                    </div>
+                                    <div className="m0-important text-sm">
+                                      <span className="m0-important font-bold f-small">{meetings.title}</span>
+                                    </div>
+                                    <div className="m0-important text-right">
+                                      <span className="title_color"> <Link href={`/home/${meetings.id}`} key={meetings.id}><h4 className="f-small text-sm">Details</h4>
+                                      </Link></span>
+                                    </div>
+                                  </div>
+                                ))}
+                                    </div>
+                                  </div>
+                                </section>
+
+                               
+
+                              </div>
+                            </div>
+                          )
+                        }
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="z-20 flex gap-x-4">
-        {/* <div className="inset-0"> */}
+
+
+
+      {/* <div className="z-20 flex gap-x-4">
         <button
           type="button"
           onClick={openModal}
@@ -76,7 +327,6 @@ export default function ProjectPanel({
         >
           Add Project
         </button>
-        {/* </div> */}
 
         <div className="z-20 flex gap-x-4">
           <Transition appear show={isOpen} as={Fragment}>
@@ -127,15 +377,15 @@ export default function ProjectPanel({
             </Dialog>
           </Transition>
         </div>
-      </div>
-      <div className="relative">
+      </div> */}
+      {/* <div className="relative">
         <Link
           href="/home/projects"
           className="absolute right-0 mt-8 flex w-32 shrink-0 items-center justify-center rounded-md bg-gray-700 text-white"
         >
           <h4 className="text-sm">View All Projects</h4>
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 }
--- components/UserCard/UserCard.jsx
+++ components/UserCard/UserCard.jsx
@@ -4,21 +4,52 @@
 
 import { Fragment, useState, useEffect } from "react";
 import { useAuthContext } from "@/contexts/AuthContext";
+import { set } from "date-fns";
 
 export default function UserCard() {
   const [currentUser, setCurrentUser] = useState(false);
   const { userSession } = useAuthContext();
-  
+  const [audioList, setAudioList] = useState([]);
+
+
   useEffect(() => {
+    setAudioList([
+      { time: '10:00 AM phone call with GC', title: 'Upload the audio' },
+      { time: '10:00 AM phone call with GC', title: 'Upload the audio' },
+      { time: '10:00 AM phone call with GC', title: 'Upload the audio' },
+      { time: '10:00 AM phone call with GC', title: 'Upload the audio' }
+    ]);
+
     if (userSession?.user?.user_metadata) {
-        setCurrentUser(userSession.user?.user_metadata)
+      setCurrentUser(userSession.user?.user_metadata)
     }
   }, [userSession])
 
   return (
-    <div className="w-full bg-emerald-300 bg-opacity-20 py-16 px-8 rounded shadow border border-stone-300 my-10 ">
+    <div className="w-full flex justify-around bg-emerald-300 bg-opacity-20 py-16 px-8 rounded shadow border border-stone-300 my-10 ">
+      <div className="my-auto">
         <h1 className="font-bold text-3xl">Welcome{currentUser?.full_name ? `, ${currentUser?.full_name}` : ''}</h1>
         <p>Check out these suggestions to start your day!</p>
+      </div>
+
+
+      <div className="grid grid-cols-2 gap-4 ">
+
+        {audioList.map((audio, index) => (
+
+        <div key={index} className="w-[296px] flex justify-between border rounded border-stone-300 px-3 py-3 bg-white" >
+          <div>
+            <h3 className="font-bold text-sm">{audio.title}</h3>
+            <p className="text-sm" >{audio.time}</p>
+          </div>
+          <div>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M9.53863 7.81555L13.5386 3.78478C13.7232 3.60017 13.7232 3.32324 13.5386 3.13863L12.9232 2.49247C12.7386 2.30786 12.4617 2.30786 12.2771 2.49247L8.24632 6.52324C8.12324 6.64632 7.93863 6.64632 7.81555 6.52324L3.78478 2.4617C3.60017 2.27709 3.32324 2.27709 3.13863 2.4617L2.49247 3.10786C2.30786 3.29247 2.30786 3.5694 2.49247 3.75401L6.52324 7.78478C6.64632 7.90786 6.64632 8.09247 6.52324 8.21555L2.4617 12.2771C2.27709 12.4617 2.27709 12.7386 2.4617 12.9232L3.10786 13.5694C3.29247 13.754 3.5694 13.754 3.75401 13.5694L7.78478 9.53863C7.90786 9.41555 8.09247 9.41555 8.21555 9.53863L12.2463 13.5694C12.4309 13.754 12.7079 13.754 12.8925 13.5694L13.5386 12.9232C13.7232 12.7386 13.7232 12.4617 13.5386 12.2771L9.53863 8.24632C9.41555 8.12324 9.41555 7.93863 9.53863 7.81555Z" fill="#747474" />
+            </svg>
+          </div>
+        </div>
+        ))}
+      </div>
     </div>
   );
 }

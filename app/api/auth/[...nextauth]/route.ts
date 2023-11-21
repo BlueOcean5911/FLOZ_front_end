c53dde127e089564503e596daef48db6bc9a1104
BlueOcean5911 <dev.freshworks@gmail.com>
2023-11-21 17:30:11 +0530

    Removed hardcoded url

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,7 @@ const handler = NextAuth({
             clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
             authorization: {
                 params: {
-                    redirect_uri: "http://localhost:3000/api/auth/callback/google",
+                    redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
                 }
             }
         }),

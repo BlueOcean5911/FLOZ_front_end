8cb64cd9edba9ac2ebe07b36877b7798e65f4e62
BlueOcean5911 <dev.freshworks@gmail.com>
2023-12-05 18:22:26 +0530

    Updated code to refresh session for google

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -20,6 +20,22 @@ const handler = NextAuth({
           session.user["id"] = token.id;
           session["accessToken"] = token.accessToken;
           session["refreshToken"] = token.refreshToken;
+
+          if (token.expires && typeof token.expires === 'number' && Date.now() > (token.expires * 1000)) {
+            try {
+              const refreshedTokens = await refreshAccessToken(token.refreshToken);
+              console.log("session refreshedTokens", refreshedTokens)
+              // Update session with new tokens
+              session["accessToken"] = refreshedTokens.access_token;
+    
+              // Update the token in the token callback
+              token.accessToken = refreshedTokens.access_token;
+            } catch (error) {
+              // Handle token refresh error
+              console.error('Error refreshing access token:', error);
+            }
+          }
+
           return session;
         },
         async jwt({ token, user, account, profile }) {
@@ -29,10 +45,33 @@ const handler = NextAuth({
           if (account) {
             token.accessToken = account.access_token;
             token.refreshToken = account.refresh_token;
+            token.expires = account.expires_at;
           }
           return token;
         },
       }
 });
+
+async function refreshAccessToken(refreshToken) {
+  const response = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+      client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      client_secret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to refresh access token');
+  }
+
+  return response.json();
+}
+
 
 export { handler as GET, handler as POST};

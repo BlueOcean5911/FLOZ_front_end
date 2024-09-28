80f40f890749973e50c0adfc033e14c135641fbf
acmicpc0614 <PleaseBugMeNot.dev@gmail.com>
2024-09-28 09:07:17 -0500

    [perf] home page

--- app/dashboard/home/layout.tsx
+++ app/dashboard/home/layout.tsx
@@ -5,16 +5,19 @@ export const metadata = {
   description: "Cost Estimator Web App",
 };
 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    <>
     <main>
       <div className="p-6 bg-slate-300 bg-opacity-20">
         {children}  
       </div>
     </main>
+    </>
   );
 }

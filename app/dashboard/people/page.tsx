a97c716bcf115f462a5a586c5648a6cc5ab29891
acmicpc0614 <PleaseBugMeNot.dev@gmail.com>
2024-09-28 09:11:10 -0500

    [fix] peopel

--- app/dashboard/people/page.tsx
+++ app/dashboard/people/page.tsx
@@ -1,7 +1,5 @@
 
 import PeopleList from "@components/People/PeopleList";
-import ListActions from "@components/People/ListActions";
-import Footer from "@components/Footer";
 
 export const revalidate = 0;
 
